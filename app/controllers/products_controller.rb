a3502b24c3c0a5dd8bd2fd3f3d7d051ae6253e71
David Gordon <davidgordon2211@gmail.com>
2020-07-20 14:13:24 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,49 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product), notice: 'Product successfully added'
+    else
+      render :new
+    end
   end
 
   def index
+    @products = Product.all
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @product.save
+      redirect_to product_path(@product), notice: 'Product successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @product.destroy
+      redirect_to products, notice: 'Product successfully removed'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_product
+    @product ||= Product.find(params[:id])
+  end
+
+  def product_params
+    params[:product].permit(:name, :price, :description, :image)
   end
 end
